3af26d72a7926aa9b971e60785d1ec76626bcc30
Jakub Litwin <92221461+litwin02@users.noreply.github.com>
2022-07-10 20:07:07 +0200

    Hello World + Chapter1 Start

---
+++ Chapter1/Chapter1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+//Testing ways of initializing values
+/*
+* { 7 } - brace direct init 
+* ( 7 ) - direct init (might not always work for all types of data)
+* my_var = 7 - copy init
+*/
+
+int main()
+{
+    int my_var = 7;
+    int my_var_brace{ 7 };
+    int my_var_dir(7);
+
+    std::cout << my_var << " " << my_var_brace << " " << my_var_dir;
+}
---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+
+//First program that outputs hello world.
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
