76333e514f5be024ba8e6d649f4ea74362b826bc
Jakub Litwin <92221461+litwin02@users.noreply.github.com>
2022-07-21 09:29:46 +0200

    Chapter 6

---
+++ Chapter6/Chapter6.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include "constants.h"
+
+//This program test inline global variables
+
+/*
+bool passOrFail()
+{
+    static int counter{0};
+    ++counter;
+    return (counter <= 3);
+}
+*/
+
+int main()
+{
+    std::cout << "Enter a radius: ";
+    int radius{};
+    std::cin >> radius;
+
+    std::cout << "The circumference is: " << 2.0 * radius * constants::pi;
+}
---
+++ Chapter6/constants.h
@@ -0,0 +1,8 @@
+#pragma once
+namespace constants 
+{
+    inline constexpr double pi{ 3.14159 };
+    inline constexpr double avogadro{ 6.0221413e23 };
+    inline constexpr double myGravity{ 9.81 };
+    //inline constexpr int max_class_size{ 35 };
+}
